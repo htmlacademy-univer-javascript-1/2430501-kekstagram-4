742039e5b8871c8393b184b13d57a449c887e56b
Keks <keks@htmlacademy.ru>
2023-10-03 09:23:42 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,22 +1,18 @@
 ///Функция для проверки длины строки
 
-const stringLength = function(checkString, maxLength) {
-  if (maxLength <= checkString.length) {
-    return true;
-  }
-  return false;
-};
+const stringLength = (checkString, maxLength) => maxLength >= checkString.length;
+
 stringLength('проверяемая строка', 20);
 stringLength('проверяемая строка', 18);
 stringLength('проверяемая строка', 10);
 
 ///Функция для проверки, является ли строка палиндромом
 
-const isPalindrome = function(string) {
-  const getString = string.replaceAll().toUpperCase();
+const isPalindrome = (string) => {
+  const getString = string.replaceAll(' ','').toUpperCase();
   let newString = '';
-  for(let i = getString.length-1; i > 0; i--) {
-    newString += getString[-1];
+  for (let i = getString.length-1; i >= 0; i--) {
+    newString += getString[i];
   }
   return newString === getString;
 };
